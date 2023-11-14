e5e828309879b321de7d4d197d9b34877759688a
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-11-14 23:43:37 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,66 @@
+    return data
+
+if __name__ == "__main__":
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+        print(f"Random Number: {item}")
+def main():
+
+    main()
+
+
+    data = generate_random_data()
+import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+    return data
+
+    for item in data:
+    main()
+import random
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+def main():
+if __name__ == "__main__":
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+
+    return data
+
+    main()
+    for item in data:
+def generate_random_data():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():    return data
+    for item in data:
+import random
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+def main():
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def generate_random_data():
+
+    main()
+def generate_random_data():
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+
+import random
+
+def main():
+if __name__ == "__main__":
+    for item in data:
+
+    main()
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
