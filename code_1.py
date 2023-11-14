ae59b25809f84168818302ba33e1f40a5d783684
DJSmith11 <142458119+DJSmith11@users.noreply.github.com>
2023-11-14 21:27:27 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,51 @@
+        print(f"Random Number: {item}")
+def generate_random_data():
+if __name__ == "__main__":
+    main()
+import random
+
+
+    data = generate_random_data()
+    for item in data:
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    return data
+    main()
+        print(f"Random Number: {item}")
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+import random
+    data = generate_random_data()
+
+
+def main():
+
+if __name__ == "__main__":
+def main():
+import random
+        print(f"Random Number: {item}")
+    main()
+def generate_random_data():
+    for item in data:
+
+
+    return data
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()        int[] data = new int[10];
+        }
+            System.out.println("Random Number: " + item);
+        }
+        for (int i = 0; i < 10; i++) {
+}
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+    }
+    public static void main(String[] args) {
+
+        for (int item : data) {
